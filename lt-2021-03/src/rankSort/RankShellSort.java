8f547fd46c3e659731a0378ad4c7f8cdb0321997
ChirisWu <1969808957@qq.com>
2021-04-11 22:22:29 +0800

    merge sort

--- lt-2021-03/src/rankSort/RankShellSort.java
+++ lt-2021-03/src/rankSort/RankShellSort.java
@@ -16,20 +16,49 @@ public class RankShellSort {
 
 
      public static void main(String[] args){
-         List<Rank> ranks = generateRanks(400000);
-         Comparator<Rank> comparator = new Comparator<Rank>(){
-             @Override
-             public int compare(Rank o1, Rank o2) {
-                 return o1.getStar().compareTo(o2.getStar());
-             }
-         };
+
+//         testShellSort(500);
+//         testMergeSort(500);
+//
+//         testShellSort(1000);
+//         testMergeSort(1000);
+//
+//         testShellSort(10000);
+//         testMergeSort(10000);
+//
+//         testShellSort(500000);
+//         testMergeSort(500000);k
+         List<Rank> ranks = generateRanks(20);
+         Comparator<Rank> comparator = (o1, o2) -> o1.getStar().compareTo(o2.getStar());
+         SortUtil<Rank> sortUtil = new SortUtil<>();
+         sortUtil.mergeSort(ranks, comparator );
+         for (Rank rank : ranks) {
+             System.out.println(rank);
+         }
+
+     }
+     static void testShellSort(int sz){
+         List<Rank> ranks = generateRanks(sz);
+         Comparator<Rank> comparator = (o1, o2) -> o1.getStar().compareTo(o2.getStar());
          long start = System.currentTimeMillis();
          SortUtil.shellSort(ranks, comparator);
-         System.out.println("sort completed, cost "+ (System.currentTimeMillis() - start));
+         System.out.println("shell sort completed,"+ ranks.size() + "cost "+ (System.currentTimeMillis() - start));
+         long start2 = System.currentTimeMillis();
+         SortUtil.shellSort(ranks, comparator);
+         System.out.println("shell sort completed,"+ ranks.size() + "cost "+ (System.currentTimeMillis() - start2));
 
+     }
+     static void testMergeSort(int sz){
+         List<Rank> ranks = generateRanks(sz);
+         Comparator<Rank> comparator = (o1, o2) -> o1.getStar().compareTo(o2.getStar());
+         SortUtil<Rank> sort = new SortUtil<>();
+         long start = System.currentTimeMillis();
+         sort.mergeSort(ranks, comparator);
+         System.out.println("merge sort completed,"+ ranks.size() + "cost "+ (System.currentTimeMillis() - start));
+         long start2 = System.currentTimeMillis();
+         sort.mergeSort(ranks, comparator);
+         System.out.println("merge sort completed,"+ ranks.size() + "cost "+ (System.currentTimeMillis() - start2));
 
-         System.out.println("set rank...");
-         setRanks(ranks);
      }
 
      private static void setRanks(List<Rank> ranks){
@@ -46,13 +75,10 @@ public class RankShellSort {
      }
 
      private static  List<Rank> generateRanks(int size){
-         long start = System.currentTimeMillis();
-         System.out.println("generating " + size + " ranks");
         List<Rank> res = new ArrayList<>(size);
         for (int i = 0; i < size; i++){
-            res.add(new Rank(new Random().nextInt(10)));
+            res.add(new Rank(new Random().nextInt(100)));
         }
-         System.out.println("generate ranks completed cost: " + (System.currentTimeMillis() - start) + "ms");
         return res;
      }
 }
--- lt-2021-03/src/rankSort/SortUtil.java
+++ lt-2021-03/src/rankSort/SortUtil.java
@@ -1,5 +1,8 @@
 package rankSort;
 
+import com.sun.scenario.effect.Merge;
+
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -7,8 +10,18 @@ import java.util.List;
  * @by ：chiris
  * @when ：Created in 2021-04-10
  */
-public class SortUtil {
+public class SortUtil<T> {
+    private T[] aux;
 
+    /**
+     * 希尔排序
+     * 时间复杂度 O(NlgN)
+     * 空间复杂度O(1)
+     * 在排序属性大量重复的场景下，在200万以下的数量级能在毫秒级完成
+     * @param list
+     * @param comparator
+     * @param <T>
+     */
     public static <T> void shellSort(List<T> list, Comparator<T> comparator){
         int N = list.size();
         int h = 1;
@@ -24,14 +37,49 @@ public class SortUtil {
             h /= 3;
         }
     }
+    public void mergeSort(List<T> list, Comparator<T> comparator){
+        aux = (T[]) new Object[list.size()];
+        mergeSort(list, 0, list.size() - 1, comparator);
 
+    }
+    private void mergeSort(List<T> list,int lo,int hi, Comparator<T> comparator){
+        if(lo == hi){
+            return;
+        }
+        int mid = lo + (hi - lo)/2;
+        mergeSort(list,lo,mid, comparator);
+        mergeSort(list,mid + 1,hi, comparator);
+        merge(list,lo,mid,hi, comparator);
+
+    }
+    public void merge(List<T> list,int lo,int mid, int hi, Comparator<T> comparator){
+        for(int k = 0;k <= hi;k++){
+            aux[k] = list.get(k);
+        }
+        int i = lo;
+        int j = mid + 1;
+        for(int k = lo; k <= hi; k++){
+            if (i > mid) {
+                list.set(k, aux[j++]);
+            }
+            else if (j > hi){
+                list.set(k, aux[i++]);
+            }
+            else if(more(comparator, aux[i], aux[j])){
+                list.set(k, aux[i++]);
+            } else{
+                list.set(k, aux[j++]);
+            }
+
+        }
+    }
 
     private static  <T> void swap(List<T> list, int i, int j){
         T temp = list.get(i);
         list.set(i, list.get(j));
         list.set(j, temp);
     }
-    private  <T>boolean less (Comparator<T> comparator, T o1, T o2){
+    private <T>boolean less (Comparator<T> comparator, T o1, T o2){
         int compare = comparator.compare(o1, o2);
         return compare < 0;
     }

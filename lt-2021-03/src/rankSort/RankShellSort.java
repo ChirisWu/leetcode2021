7db366e68aaf86cf389b638eea4709288a68f348
ChirisWu <1969808957@qq.com>
2021-04-11 19:01:12 +0800

    set Rank

--- lt-2021-03/src/rankSort/RankShellSort.java
+++ lt-2021-03/src/rankSort/RankShellSort.java
@@ -16,18 +16,33 @@ public class RankShellSort {
 
 
      public static void main(String[] args){
-         List<Rank> ranks = generateRanks(100000);
+         List<Rank> ranks = generateRanks(400000);
          Comparator<Rank> comparator = new Comparator<Rank>(){
              @Override
              public int compare(Rank o1, Rank o2) {
                  return o1.getStar().compareTo(o2.getStar());
              }
          };
+         long start = System.currentTimeMillis();
          SortUtil.shellSort(ranks, comparator);
-         for (int i = 0; i < 100; i++) {
-             System.out.println(ranks.get(i));
-         }
+         System.out.println("sort completed, cost "+ (System.currentTimeMillis() - start));
 
+
+         System.out.println("set rank...");
+         setRanks(ranks);
+     }
+
+     private static void setRanks(List<Rank> ranks){
+         long start = System.currentTimeMillis();
+         int seed = 0;
+         ranks.get(0).setRank(1);
+         for (int i = 1; i < ranks.size(); i++){
+             if (ranks.get(i - 1).getStar() == ranks.get(i).getStar()){
+                 seed ++;
+             }
+             ranks.get(i).setRank(i + 1 - seed);
+         }
+         System.out.println("set rank completed cost " + (System.currentTimeMillis() - start));
      }
 
      private static  List<Rank> generateRanks(int size){
@@ -35,7 +50,7 @@ public class RankShellSort {
          System.out.println("generating " + size + " ranks");
         List<Rank> res = new ArrayList<>(size);
         for (int i = 0; i < size; i++){
-            res.add(new Rank(new Random().nextInt(5)));
+            res.add(new Rank(new Random().nextInt(10)));
         }
          System.out.println("generate ranks completed cost: " + (System.currentTimeMillis() - start) + "ms");
         return res;
